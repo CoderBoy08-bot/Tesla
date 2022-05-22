2d3054c6353ec23ecb9949607bc96807063419c3
CoderBoy08 <rajvigneshwark@gmail.com>
2022-05-22 17:19:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let firstName = "Raj";
+let lastName = "Vigneshwar";
+let combinedName = firstName + " " + lastName;
+console.log(combinedName);
+
+if (combinedName === "Raj") {
+  console.log("hello raj");
+} else {
+  alert("error occured");
+}
